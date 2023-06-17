ab7b1b23222753b59d06f9a272ac32f84f894225
Sam Alex Koshy <samuel.alex.koshy@gmail.com>
2023-06-17 21:34:00 +0530

    feat: identify error made, check if enough saves

--- src/samperium/monitor.py
+++ src/samperium/monitor.py
@@ -5,8 +5,10 @@ from nextcord import Guild, Member, Message, Role
 from nextcord.ext.commands import Cog, Bot
 from nextcord.utils import get
 
-COUNTING_ID = 510016054391734273
+COUNTING_BOT = 510016054391734273
 NUMSELLI_BOT = 726560538145849374
+CRAZY_BOT = 935408554997874798
+
 COUNTABLE = 1119485526009974805
 NUMSELLI_COUNTABLE = 1119488910632943656
 CRAZY_COUNTABLE = 1119488960587112448
@@ -15,7 +17,12 @@ pp = PrettyPrinter(indent=4)
 
 
 async def give_count_permission(
-    saves: int, role: Role, counter: Member, guild: Guild, message: Message
+    saves: int,
+    role: Role,
+    counter: Member,
+    guild: Guild,
+    message: Message,
+    bot: int,
 ):
     """Giving permission to count in respective bots"""
     if saves >= 1:
@@ -32,7 +39,7 @@ async def give_count_permission(
                 reason="Has enough saves",
             )
             await message.channel.send(
-                f"{counter.mention} can now count in <#{1017139366100992042}>"
+                f"{counter.mention} can now count with <@{bot}>."
             )
     else:
         if role in counter.roles:
@@ -40,7 +47,9 @@ async def give_count_permission(
                 role,
                 reason="Doesn't have enough saves",
             )
-            await message.channel.send("Not enough saves")
+            await message.channel.send(
+                f"{counter.mention} doesn't have enough saves to count with <@{bot}>."
+            )
         else:
             reaction = get(guild.emojis, name="nekogasp")
             if reaction is not None:
@@ -48,7 +57,7 @@ async def give_count_permission(
 
 
 def counting_check(m: Message):
-    return m.author.id == COUNTING_ID
+    return m.author.id == COUNTING_BOT
 
 
 class Monitor(Cog):
@@ -90,17 +99,12 @@ class Monitor(Cog):
                 message.author,
                 message.guild,
                 msg,
+                COUNTING_BOT,
             )
 
         # * c!user
-        elif message.content.startswith("c!user"):
-            msg = await self.bot.wait_for("message", check=counting_check)
-            if not isinstance(msg, Message):
-                return
-            if len(msg.embeds) != 1:
-                return
-
-            embed_content = msg.embeds[0].to_dict()
+        elif message.author.id == COUNTING_BOT and len(message.embeds) == 1:
+            embed_content = message.embeds[0].to_dict()
             user_name = embed_content.get("title")
             if user_name is None:
                 return
@@ -121,10 +125,70 @@ class Monitor(Cog):
                 countable,
                 user,
                 message.guild,
-                msg,
+                message,
+                COUNTING_BOT,
             )
 
+        # * Numselli
         elif message.author.id == NUMSELLI_BOT and len(message.embeds) == 1:
+            embed_content = message.embeds[0].to_dict()
+            embed_title = embed_content.get("title")
+            if embed_title is None:
+                return
+
+            # * Numselli user
+            if embed_title.startswith("Stats"):
+                user_name = embed_title.split("Stats for ")[1]
+                if user_name is None:
+                    return
+                user = message.guild.get_member_named(user_name)
+                if user is None:
+                    return
+                if "fields" not in embed_content:
+                    return
+                embed_field = embed_content["fields"][0]
+                field_value = embed_field["value"]
+                saves_str = field_value.split("Saves left: ")[1]
+                saves = int(findall("\d", saves_str)[0])  # type: ignore
+                countable = message.guild.get_role(NUMSELLI_COUNTABLE)
+                if countable is None:
+                    return
+                await give_count_permission(
+                    saves,
+                    countable,
+                    user,
+                    message.guild,
+                    message,
+                    NUMSELLI_BOT,
+                )
+
+            # * Numselli error
+            elif embed_title.startswith("Save Used"):
+                embed_description = embed_content.get("description")
+                if embed_description is None:
+                    await message.channel.send("Could not read embed")
+                    return
+                numbers = findall("\d+", embed_description)  # type: ignore
+                user_id = int(numbers[0])
+                saves = int(numbers[1])
+                user = message.guild.get_member(user_id)
+                if user is None:
+                    await message.channel.send("Could not identify the user")
+                    return
+                countable = message.guild.get_role(NUMSELLI_COUNTABLE)
+                if countable is None:
+                    return
+                await give_count_permission(
+                    saves,
+                    countable,
+                    user,
+                    message.guild,
+                    message,
+                    NUMSELLI_BOT,
+                )
+
+        # * Crazy Counting user
+        elif message.author.id == CRAZY_BOT and len(message.embeds) == 1:
             embed_content = message.embeds[0].to_dict()
             embed_title = embed_content.get("title")
             if embed_title is None:
@@ -132,16 +196,16 @@ class Monitor(Cog):
             user_name = embed_title.split("Stats for ")[1]
             if user_name is None:
                 return
-            user = message.guild.get_member_named(user_name)
+            user = message.guild.get_member_named(user_name.split("#")[0])
             if user is None:
                 return
             if "fields" not in embed_content:
                 return
             embed_field = embed_content["fields"][0]
             field_value = embed_field["value"]
-            saves_str = field_value.split("Saves left: ")[1]
-            saves = int(findall("\d", saves_str)[0])
-            countable = message.guild.get_role(NUMSELLI_COUNTABLE)
+            saves_str = field_value.split("Saves: ")[1]
+            saves = int(findall("\d", saves_str)[0])  # type: ignore
+            countable = message.guild.get_role(CRAZY_COUNTABLE)
             if countable is None:
                 return
             await give_count_permission(
@@ -150,8 +214,63 @@ class Monitor(Cog):
                 user,
                 message.guild,
                 message,
+                CRAZY_BOT,
             )
             # await message.channel.send(f"{field_value}")
+
+        # * Error from counting
+        if (
+            message.author.id == COUNTING_BOT
+            and message.content is not None
+            and len(message.embeds) == 0
+            and "You have" in message.content
+        ):
+            content = message.content
+            numbers = findall("\d+", content)  # type: ignore
+            user_id = int(numbers[0])
+            saves = int(numbers[2])
+            user = message.guild.get_member(user_id)
+            if user is None:
+                await message.channel.send("Couldn't find who made the mistake")
+                return
+            countable = message.guild.get_role(COUNTABLE)
+            if countable is None:
+                return
+            await give_count_permission(
+                saves,
+                countable,
+                user,
+                message.guild,
+                message,
+                COUNTING_BOT,
+            )
+
+        # * Error from crazy counting
+        if (
+            message.author.id == CRAZY_BOT
+            and message.content is not None
+            and len(message.embeds) == 0
+            and "has used a save" in message.content
+        ):
+            content = message.content
+            numbers = findall("\d+", content)  # type: ignore
+            user_id = int(numbers[0])
+            saves = int(numbers[1])
+            user = message.guild.get_member(user_id)
+            if user is None:
+                await message.channel.send("Couldn't find who made the mistake")
+                return
+            countable = message.guild.get_role(CRAZY_COUNTABLE)
+            if countable is None:
+                return
+            await give_count_permission(
+                saves,
+                countable,
+                user,
+                message.guild,
+                message,
+                COUNTING_BOT,
+            )
 
 
 def setup(bot: Bot):
