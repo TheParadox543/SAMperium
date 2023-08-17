cd1a7a35a3c4439d488c7709682d4d2090d2da77
Sam Alex Koshy <samuel.alex.koshy@gmail.com>
2023-08-17 18:06:46 +0530

    feat: create rankup method
    Bot now tells how much is required to rank up
    Error of reading crazy embed fixed
    Palindrome reaction added

---
+++ src/samperium/database.py
@@ -0,0 +1,58 @@
+import certifi
+from datetime import datetime
+from os import getenv
+
+from pymongo import MongoClient
+from dotenv import load_dotenv
+from nextcord import Member
+
+ca = certifi.where()
+load_dotenv()
+
+CLUSTER = MongoClient(getenv("DB_LOCAL_TOKEN"), tlsCAFile=ca)
+# CLUSTER = MongoClient(getenv("DB_USA_TOKEN"), tlsCAFile=ca)
+
+DB = CLUSTER["samperium"]
+user_collection = DB["user_collection"]
+
+
+def save_data(
+    user: Member,
+    bot_name: str,
+    rate: float,
+    correct: int,
+    wrong: int,
+    time_now: datetime,
+):
+    """Save the bot related data for a user
+
+    Args:
+        user (Member): The counter details
+        bot_name (str): The bot name
+    """
+
+    result = user_collection.update_one(
+        {"_id": f"{user.id}"},
+        {
+            "$set": {
+                "name": user.name,
+                f"{bot_name}.correct": correct,
+                f"{bot_name}.wrong": wrong,
+                f"{bot_name}.rate": rate,
+                f"{bot_name}.time": time_now,
+            }
+        },
+        upsert=True,
+    )
+    return result.modified_count
+
+
+def get_data(user: Member):
+    """Get the users data
+
+    Args:
+        user (Member): The user who requested the data
+    """
+
+    result = user_collection.find_one({"_id": f"{user.id}"})
+    return result
--- src/samperium/monitor.py
+++ src/samperium/monitor.py
@@ -1,9 +1,15 @@
-from re import match, search, findall
+from datetime import datetime, timedelta
+from decimal import ROUND_UP, Decimal, getcontext
+from math import sqrt
 from pprint import PrettyPrinter
+from re import findall, match, search
+from typing import cast
 
-from nextcord import Guild, Member, Message, Role
-from nextcord.ext.commands import Cog, Bot, command, Context, is_owner
+from nextcord import Embed, Guild, Member, Message, Role
+from nextcord.ext.commands import Bot, Cog, Context, command, is_owner
 from nextcord.utils import get
+
+from database import get_data, save_data
 
 COUNTING_BOT = 510016054391734273
 NUMSELLI_BOT = 726560538145849374
@@ -12,6 +18,8 @@ CRAZY_BOT = 935408554997874798
 COUNTABLE = 1017143547541078066
 NUMSELLI_COUNTABLE = 1115578157727219712
 CRAZY_COUNTABLE = 1115578109056516136
+
+PRIME_CHANNEL_ID = 1020461234115579934
 
 pp = PrettyPrinter(indent=4)
 
@@ -60,6 +68,25 @@ def counting_check(m: Message):
     return m.author.id == COUNTING_BOT
 
 
+def prime(num: int):
+    f = False
+    for i in range(3, int(sqrt(num)) + 1, 2):
+        if num % i == 0:
+            f = True
+    return f
+
+
+def next_prime(num: int):
+    f = True
+    while f:
+        if num % 2 == 1:
+            num += 2
+        else:
+            num += 1
+        f = prime(num)
+    return num
+
+
 class Monitor(Cog):
     """Monitor the different bots and give permissions"""
 
@@ -74,6 +101,20 @@ class Monitor(Cog):
         if not isinstance(message.guild, Guild):
             return
 
+        # * Message from users
+        if len(message.content) > 1 and message.content == message.content[::-1]:
+            rev_reaction = get(message.guild.emojis, name="alternating_peek")
+            if rev_reaction is not None:
+                await message.add_reaction(rev_reaction)
+
+        # if message.channel.id == PRIME_CHANNEL_ID:
+        #     try:
+        #         number = int(message.content)
+        #     except ValueError:
+        #         return
+        #   next_prime_number = next_prime(number)
+        #   await message.channel.send(f"`Next: {next_prime_number}`")
+
         # * c!vote
         if message.content.startswith("c!vote"):
             msg = await self.bot.wait_for("message", check=counting_check)
@@ -86,7 +127,7 @@ class Monitor(Cog):
             if "description" not in embed_content:
                 return
             embed_description = embed_content["description"]
-            saves_str = findall("\d+", embed_description)  # type: ignore
+            saves_str = findall(r"\d+", embed_description)
             if len(saves_str) < 1:
                 return
             saves = int(saves_str[0])
@@ -110,21 +151,28 @@ class Monitor(Cog):
             if len(msg.embeds) != 1:
                 return
 
+            numbers = findall(r"\d+", message.content)
+            if len(numbers) == 1:
+                user_id = int(numbers[0])
+                user = message.guild.get_member(user_id)
+            else:
+                user = message.author
+
             embed_content = msg.embeds[0].to_dict()
-            user_name = embed_content.get("title")
-            if user_name is None:
-                return
-            await message.channel.send(user_name.split("#")[0])
-            user = message.guild.get_member_named(user_name.split("#")[0])
-            await message.channel.send(f"Counter is: {user}")
             if user is None:
+                await message.channel.send("Could not find the user")
                 return
             if "fields" not in embed_content:
                 return
             embed_field = embed_content["fields"][0]
             field_value = embed_field["value"]
-            saves_str = field_value.split("Saves: ")[1]
-            saves = int(findall("\d", saves_str)[0])  # type: ignore
+            field_value = field_value.replace(",", "")
+            number_list = findall(r"[0-9]+\.*[0-9]*", field_value)
+            rate = float(number_list[0])
+            correct = int(number_list[1])
+            wrong = int(number_list[2])
+            save_data(user, "counting", rate, correct, wrong, message.created_at)
+            saves = int(number_list[5])
             countable = message.guild.get_role(COUNTABLE)
             if countable is None:
                 return
@@ -157,7 +205,7 @@ class Monitor(Cog):
                 embed_field = embed_content["fields"][0]
                 field_value = embed_field["value"]
                 saves_str = field_value.split("Saves left: ")[1]
-                saves = int(findall("\d", saves_str)[0])  # type: ignore
+                saves = int(findall(r"\d", saves_str)[0])
                 countable = message.guild.get_role(NUMSELLI_COUNTABLE)
                 if countable is None:
                     return
@@ -176,7 +224,7 @@ class Monitor(Cog):
                 if embed_description is None:
                     await message.channel.send("Could not read embed")
                     return
-                numbers = findall("\d+", embed_description)  # type: ignore
+                numbers = findall(r"\d+", embed_description)
                 user_id = int(numbers[0])
                 saves = int(numbers[1])
                 user = message.guild.get_member(user_id)
@@ -201,6 +249,8 @@ class Monitor(Cog):
             embed_title = embed_content.get("title")
             if embed_title is None:
                 return
+            if "Stats for" not in embed_title:
+                return
             user_name = embed_title.split("Stats for ")[1]
             if user_name is None:
                 return
@@ -212,7 +262,7 @@ class Monitor(Cog):
             embed_field = embed_content["fields"][0]
             field_value = embed_field["value"]
             saves_str = field_value.split("Saves: ")[1]
-            saves = int(findall("\d", saves_str)[0])  # type: ignore
+            saves = int(findall(r"\d", saves_str)[0])
             countable = message.guild.get_role(CRAZY_COUNTABLE)
             if countable is None:
                 return
@@ -233,7 +283,7 @@ class Monitor(Cog):
             and "You have" in message.content
         ):
             content = message.content
-            numbers = findall("\d+", content)  # type: ignore
+            numbers = findall(r"\d+", content)
             user_id = int(numbers[0])
             user = message.guild.get_member(user_id)
             if user is None:
@@ -263,7 +313,7 @@ class Monitor(Cog):
             and "has used a" in message.content
         ):
             content = message.content
-            numbers = findall("\d+", content)  # type: ignore
+            numbers = findall(r"\d+", content)
             user_id = int(numbers[0])
             user = message.guild.get_member(user_id)
             if user is None:
@@ -291,10 +341,74 @@ class Monitor(Cog):
     @is_owner()
     async def remove_roles(self, ctx: Context, role: Role):
         """Remove all users from the role"""
+
         for member in role.members:
-            # if member.name == "paradox543":
             await member.remove_roles(role, reason="Clearing roles from users")
             await ctx.send(f"{role} removed from {member.name}")
+
+    @command(name="rankup", aliases=["ru"])
+    async def rankup_cmd(self, ctx: Context, member: Member | None = None):
+        """Shows the number of counts required to increase stats"""
+
+        user = cast(Member, member or ctx.author)
+        msg = ""
+
+        user_data = get_data(user)
+        if user_data is None:
+            await ctx.send("Run counting commands first!")
+            return
+
+        og_post = user_data.get("counting")
+        if (
+            og_post is not None
+            and og_post.get("time") + timedelta(minutes=30) > datetime.utcnow()
+        ):
+            correct = Decimal(og_post.get("correct", 0))
+            wrong = Decimal(og_post.get("wrong", 0))
+            total = correct + wrong
+            rate = (correct / total).quantize(Decimal("1.00000"))
+            if rate >= Decimal("0.9998"):
+                msg += "`counting`: The bot can't calculate the number of counts "
+                msg += "you need to rank up\n"
+            else:
+                new_rate = rate + Decimal("0.000005")
+                x = ((new_rate * total - correct) / (Decimal("1") - new_rate)).quantize(
+                    Decimal("1"), ROUND_UP
+                )
+                new_cor = correct + x
+                new_rate = (new_rate * 100).quantize(Decimal("10.000"), ROUND_UP)
+                msg += f"`counting`: Rank up to {new_rate}% at **{new_cor}**. "
+                msg += f"You need ~**{x}** more numbers.\n"
+
+        classic_post = user_data.get("classic")
+        if (
+            classic_post is not None
+            and classic_post.get("time") + timedelta(minutes=30) > datetime.utcnow()
+        ):
+            correct = Decimal(classic_post.get("correct", 0))
+            wrong = Decimal(classic_post.get("wrong", 0))
+            total = correct + wrong
+            rate = (correct / total).quantize(Decimal("1.00000"))
+            if rate >= Decimal("0.9998"):
+                msg += "`classic`: The bot can't calculate the number of counts "
+                msg += "you need to rank up\n"
+            else:
+                getcontext().prec = 8
+                new_rate = rate + Decimal("0.000005")
+                x = ((new_rate * total - correct) / (1 - new_rate)).quantize(
+                    Decimal("1"), ROUND_UP
+                )
+                new_cor = correct + x
+                new_rate = (new_rate * 100).quantize(Decimal("10.000"))
+                msg += f"`classic`: Rank up to {new_rate}% at **{new_cor}**. "
+                msg += f"You need ~**{x}** more numbers.\n"
+
+        if msg == "":
+            msg = "Run counting commands first!"
+
+        title_msg = f"Rank up stats for {user.display_name}"
+        embedVar = Embed(title=title_msg, description=msg)
+        await ctx.send(embed=embedVar)
 
 
 def setup(bot: Bot):
